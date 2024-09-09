d7798cecac5409cb49d97588fd5c727a422c1725
olusola akinsulere <akinsulereolusola@gmail.com>
2024-09-09 22:51:41 +0100

    bypass proxy

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 import qs from 'qs';
 
 //const API_BASE_URL = '/api'; 
-const API_BASE_URL = 'http://135.237.65.229:8080'
+//const API_BASE_URL = 'http://135.237.65.229:8080'
+
+const API_BASE_URL = 'https://ujaimwndnztuigitjxr44mztca0dopro.lambda-url.eu-west-2.on.aws'
 
 export const fetchToken = async () => {
     try {
